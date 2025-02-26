b3f482f3e4fa0122191953270532d4d185bbfc0c
prosper615 <prosperchiderachukwu@gmail.com>
2025-02-26 05:48:43 -0800

    I read crytography for fun

--- DH.js
+++ DH.js
@@ -127,16 +127,10 @@ let TheBase = 2
 
 let TheExponent = 1
 
-// let IterationCount = 0
-
-// it seems like am gonna put the results into an array in order to keep a count of them
-
 
 let PutInArray = [];
 
 let EquatingPowerK 
-
-let EquatingPowerM
 
 
 
@@ -152,72 +146,80 @@ while( P % (TheBase ** TheExponent ) === 0 ){
 
    TheExponent++
 
-
-
      
 }
 
- 
 EquatingPowerK = PutInArray.length
 
-console.log(EquatingPowerK)
-
-EquatingPowerM = EquatingPowerK + 1
-
-// console.log( EquatingPowerM)
 
 let TheRemainder = P / TheBase ** EquatingPowerK
 
-console.log( TheRemainder)
 
+// P should be equal to ... as shown : P === 2**EquatingPowerK * TheRemainder
 
-
-
-// Now checking for eqaulity based on a formula in Miller-Rabin computation
-
-if( P === 2**EquatingPowerK * TheRemainder ){
-
-
-    console.log( "Am a computer wizard")
-
-
-}
-
-
-else {
-
-console.log( "I love software engineering and also to hack on cool stuffs ")
- 
-
-}
-
-
-// Now there is more steps to take here
 
 // Via the formula for computing Miller-Rabin primality test, we must find a number which satisfies below
 
 // let call the number T.  The condition to be fuffilled : 1 < T < n-1
 
-//  Implementing the condnition above
+//  Implementing the condition  above with the code below
 
-let T = Math.floor(Math.random() * (P - 2)) + 2
+// let T = Math.floor(Math.random() * (P - 2)) + 2
 
-if( T < P){
+let T = 2  // using 2 tempoarily to confirm some certain calculations
 
-console.log("The test  have been passed " )
 
+// Now am going into the next step of checking for prime using Miller-Rabin algorithm
+
+
+let CompositeOrPrime = T ** TheRemainder  %  N
+
+console.log(CompositeOrPrime);
+
+
+// Now implementing the condition that checks if the number is a prime numbe ror a coprime
+
+if( CompositeOrPrime % N == 1){
+
+console.log("This is a composite number " )
+
+
+}
+
+else if( CompositeOrPrime % N == -1){
+
+
+console.log( "This is probably a prime numbber " + "\n" + "This is due to the presence of a kind of number could pass the primality test known as Carmicheals number")
 
 }
 
 else{
 
-console.log( "Let's find another approach")
+
+while( CompositeOrPrime !== 1 || CompositeOrPrime !== -1){
+
+
+let CompositeOrPrimeSwap = CompositeOrPrime **2  % N
+
+
+console.log(CompositeOrPrimeSwap)
+
+
+CompositeOrPrimeSwap = CompositeOrPrime
+
+
 
 
 
 }
 
-// Now am going into the next step of checking for prime using Miller-Rabin algorithm
+
+}
+
+
+
+
+ 
 
 
 
@@ -236,39 +238,14 @@ Miller_Rabin ( 561 )
 
 // What is the difference and effect of the two different approach in a code below
 
+// console.log(x**y);
 
-
-// console.log(2**3);
-
-// console.log( Math.pow( 2, 3))
-
-
-
-// Testing something
-
-const NotBeyond = 8;
-
-const TestNotBeyond = Math.floor( Math.random() * (NotBeyond - 2)) + 2
-
-console.log( TestNotBeyond)
-
-
-// I can't continue coding now because i have to first learn how to get  the
-
-// primitive root of a prime number and also finish up on that Miller-Rabin algorithm
+// console.log( Math.pow( x, y)
 
 
 
-// My text project should be implementing a crytographic concept in ts. Then do something in React
 
-//  And all in into blockchain development
-
-// For  now let me go and read my book, crytography to be precise 
-
-
-// Now i have learnt how to check for the primitive root
-
-// Now Implementing it
+// Checking if the selected number is really the primitive root below
 
 
 const  AdaPrimitive = document.getElementById("AdaPrimitive")
@@ -291,15 +268,146 @@ console.log( GetObiPrimitiveValue)
 
 console.log(GetAdaPrimitiveValue)
 
+if( GetAdaPrimitiveValue !== GetObiPrimitiveValue ){
 
+console.log("They did'nt choose the same primitive root for the prime")
 
-    
+}
+
+else{
+
+console.log("Your primitive roots are the same")
+
 }
 
 
 
 
 
+
+
+}
+
+
+
+// Now its time to check if the number choosen is really the primitive root of the prime number
+
+
+
+function ConstructPrimitive () { 
+  
+let GetObiPrimeNumber = ObiPrimeNumber.value;
+
+let GetAdaPrimeNumber =  AdaPrimeNumber.value;
+
+let IfTheyareEqual
+
+
+// console.log(GetAdaPrimeNumber)
+
+// console.log(GetObiPrimeNumber)
+
+
+// Just tempoarily placing this here, will implement it up there.
+
+// I think checking the primitive root would be after we have checked if they have the same prime number
+
+
+if ( GetObiPrimeNumber == GetAdaPrimeNumber){
+
+IfTheyareEqual = GetObiPrimeNumber   // i can  also equate it to AdaPrimeNumber since they are equal
+
+
+// console.log(IfTheyareEqual)
+
+// If the choosen primitive roots are equaal then this has to happen to it
+
+
+let GetObiPrimitiveValue = ObiPrimitive.value
+
+let InitialIterating = 1   // This is as a result of the formula
+
+let InitialIteratingStop = IfTheyareEqual - 1
+
+
+let PushSeePrimitiveResults = []
+
+
+while( InitialIterating <= InitialIteratingStop){
+
+
+
+    let SeePrimitive = GetObiPrimitiveValue ** InitialIterating % GetObiPrimeNumber
+
+
+     PushSeePrimitiveResults.push(SeePrimitive)
+
+    console.log( "Checking primitive root ")
+
+
+    InitialIterating++
+
+
+
+
+}
+
+
+console.log(PushSeePrimitiveResults)
+
+let DuplicateCompare = new Set(PushSeePrimitiveResults)
+
+console.log( DuplicateCompare)
+
+
+
+if( PushSeePrimitiveResults.length == DuplicateCompare.size){
+
+console.log( "This is the primitive prime")
+
+
+}
+
+else{
+
+console.log( "This is not the realative prime")
+
+}
+
+
+
+}
+
+
+
+
+}
+
+
+// Testing out something out rn
+
+
+console.log( "Am a computer wizard " + "\n" + "Am a software engineering wizard" + "\n" + "Am a blockchain wizard")
+
+
+
+
+let  TryIt = 10;
+
+let TryIt2 = 6;
+
+
+while( TryIt + TryIt2 < 100){
+
+console.log("Until it's up to 100")
+
+TryIt +=  TryIt2
+
+TryIt2 = TryIt;
+
+
+
+}
 
 
 
